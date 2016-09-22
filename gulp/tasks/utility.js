1b1d4ca051383d434eb49385d781e4a9b127a0db
peterberwind <peterberwind@gmail.com>
2016-09-21 21:16:41 -0400

    update utility.js gulp file

--- gulp/tasks/utility.js
+++ gulp/tasks/utility.js
@@ -2,19 +2,26 @@ var watch = require('gulp-watch');
 
 module.exports = function(gulp, config) {
 	'use strict';
-
-	// Moves Skeleton theme assets ./theme/assets
+	var runSequence = require('run-sequence').use(gulp);
+	// a handy clean task papu
+	gulp.task('clean', function() {
+		return del([
+			config.destination
+		]);
+	});
+	// Moves theme assets ./theme/assets
 	gulp.task('timber', function() {
 		return gulp.src('./assets/timber/**')
 			.pipe(gulp.dest(config.destination));
 	});
-
 	gulp.task('watch', function() {
 	  gulp.watch(config.sources.scss, ['scss']);
 	  gulp.watch(config.sources.scripts, ['scripts']);
 	  gulp.watch(config.sources.images, ['images']);
 	  gulp.watch(config.sources.fonts, ['fonts']);
 	});
-
-	gulp.task('default', ['timber', 'shopify:watch', 'watch']);
+	// on gulp 3 we have to use run-sequence otherwise results may vary
+	gulp.task('default', function() {
+		return runSequence('clean','timber', 'shopify:watch', 'watch');
+	});
 };
