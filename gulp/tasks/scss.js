7f389a0ba4f8231298f34dce19d1dc53f415eb31
peterberwind <peterberwind@gmail.com>
2016-09-21 21:08:07 -0400

    update scss tasks

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,35 +1,26 @@
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var cssnano = require('gulp-cssnano');
-var changed = require('gulp-changed');
-var plumber = require('gulp-plumber');
-var bulkSass = require('gulp-sass-bulk-import');
-var concat = require('gulp-concat');
-var gulpif = require('gulp-if');
-var rename = require('gulp-rename');
+var $ = require('gulp-load-plugins')();
 
 module.exports = function(gulp, config) {
+
 	'use strict';
 
 	gulp.task('scss', function() {
 		return gulp.src(config.sources.scss)
-			.pipe(plumber())
-			.pipe(bulkSass())
-			.pipe(changed(config.destination))
-			.pipe(gulpif(config.sourcemaps === true, sourcemaps.init()))
-			// Compiles synchronously
-			.pipe(sass.sync({
+			.pipe($.plumber())
+			.pipe($.if(config.sourcemaps === true, $.sourcemaps.init()))
+			.pipe($.changed(config.destination))
+			.pipe($.sass.sync({
 				includePaths: config.modules.scss
 			})
-			.on('error', sass.logError))
-			.pipe(autoprefixer({
+			.on('error', $.sass.logError))
+			.pipe($.autoprefixer({
 				browsers: config.browsers
 			}))
-			.pipe(concat('main.css'))
-			.pipe(gulpif(config.minify === true, cssnano()))
-			.pipe(gulpif(config.sourcemaps === true, sourcemaps.write()))
-			.pipe(rename('main.css.liquid'))
+			.pipe($.groupCssMediaQueries())
+			.pipe($.csscomb())
+			.pipe($.if(config.minify === true, $.cssnano() ))
+			.pipe($.rename('main.css.liquid'))
+			.pipe($.if(config.sourcemaps === true, $.sourcemaps.write('./')))
 			.pipe(gulp.dest(config.destination));
 	});
 };
