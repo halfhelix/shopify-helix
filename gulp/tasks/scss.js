b272078c58034cc19e76631add66fc6980332a1f
peterberwind <peterberwind@gmail.com>
2016-11-12 16:34:27 -0500

    getting rid of duplicate Css

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -5,23 +5,21 @@ module.exports = function(gulp, config) {
 	'use strict';
 
 	gulp.task('scss', function() {
-		return gulp.src(config.sources.scss)
-			.pipe($.plumber())
-			.pipe($.if(config.sourcemaps === true, $.sourcemaps.init()))
-			.pipe($.changed(config.destination))
-			.pipe($.sass.sync({
-				includePaths: config.modules.scss
-			})
-			.on('error', $.sass.logError))
-			.pipe($.autoprefixer({
-				browsers: config.browsers
-			}))
-			.pipe($.groupCssMediaQueries())
-			.pipe($.csscomb())
-			.pipe($.if(config.minify === true, $.cssnano() ))
-			.pipe($.rename('main.css.liquid'))
-			.pipe($.if(config.sourcemaps === true, $.sourcemaps.write('./')))
-			.pipe(gulp.dest(config.destination));
-	});
+    return gulp.src('./assets/scss/style.scss')
+      .pipe($.plumber())
+      .pipe($.changed(config.destination))
+      .pipe($.sass.sync({
+          includePaths: config.modules.scss
+      })
+      .on('error', $.sass.logError))
+      .pipe($.autoprefixer({
+          browsers: config.browsers
+      }))
+      .pipe($.groupCssMediaQueries())
+      .pipe($.csscomb())
+      .pipe($.cssnano() )
+      .pipe($.rename('main.css.liquid'))
+      .pipe(gulp.dest(config.destination));
+    });
 
 };
